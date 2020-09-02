c8fb67ef345a652326274cc7d39987f2b50eaf13
dsntos <thinkerston.ml@gmail.com>
2020-09-02 08:36:38 -0400

    -add script for solve and operator

--- 04-Perceptron-ajuste-pesos-or-operator.py
+++ 04-Perceptron-ajuste-pesos-or-operator.py
@@ -2,7 +2,7 @@
 import numpy as np
 
 entradas  = np.array([[0,0], [0, 1], [1, 0], [1, 1]])
-saidas = np.array([0,0,0,1])
+saidas = np.array([0,1,1,1])
 
 pesos = np.array([0.0, 0.0])
 taxaAprendizagem = 0.1
@@ -33,4 +33,6 @@ def treinar():
                 
         print(f'total Erros: {erroTotal}')
 
+
 treinar()
+
